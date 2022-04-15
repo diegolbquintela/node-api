b6ac5bf353e7084215086734f943e805fdb8efc7
Diego Quintela <diegolucasq@gmail.com>
2022-04-14 21:37:30 -0400

    Full scale update: improved the views files, controllers and routers logic.

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-const http = require('http');
 const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const rootDir = require('./util/path');
+const errorController = require('./controllers/error');
 
 //initializing an express application
 const app = express();
 
-const adminData = require('./routes/admin');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public'))); //serving css statically
 
 //middlewares
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 // handling wrong routes
-app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-});
+app.use(errorController.get404);
 
 app.listen(process.env.PORT || 8000);
